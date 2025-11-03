89a21c05ffb37ef2ae07c6e5b4309e019134d832
oldwestbury <bestcodeguru@gmail.com>
2025-11-03 23:57:11 +0000

    a:

--- code/bmp/test.cpp
+++ code/bmp/test.cpp
@@ -1,17 +1,17 @@
 #include "bmp.hpp"
 #include <iostream>
-
-void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b){
-    for(int i=0;i<l;i++){
-        for(int ii=0;ii<b;ii++){
-        bmp.set_pixel(i+x, y+ii, 0,0,0);
-        bmp.set_pixel(i+x, y+h-ii, 0,0,0);
+#include <time.h>
+void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b, color c ){
+    for(int i=0;i<=l;i++){
+        for(int ii=0;ii<=b;ii++){
+        bmp.set_pixel(i+x, y+ii, c.r,c.g,c.b);
+        bmp.set_pixel(i+x, y+h-ii, c.r, c.g,c.b);
         }
     }
-    for(int j=0;j<h;j++){
-        for(int ii=0;ii<b;ii++){
-            bmp.set_pixel(x+ii, y+j, 0,0,0);
-            bmp.set_pixel(x+l-ii, y+j, 0,0,0);
+    for(int j=0;j<=h;j++){
+        for(int ii=0;ii<=b;ii++){
+            bmp.set_pixel(x+ii, y+j, c.r,c.g,c.b);
+            bmp.set_pixel(x+l-ii, y+j,c.r, c.g,c.b);
         }
     }
 }
@@ -19,18 +19,20 @@ void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b){
 int main() {
     
     BMP bmp(500, 500); 
-
+     srand(static_cast<unsigned int>(time(0)));
+    color c(0,255,0);
     for(int i=0;i<5;i++){
         uint8_t x=rand()%500;
         uint8_t y=rand()%500;
         uint8_t l=rand()%500;
         uint8_t h=rand()%500;
         
-        draw_rectangle(bmp,x,y,l,h,5);
+        draw_rectangle(bmp,x,y,l,h,rand()%10,c);
 
     }
     
-    
+     draw_rectangle(bmp,1,1,500,500,10,color{255,0,0});
+
     bmp.write("rect.bmp"); // Save the image to a file
 
     return 0;
