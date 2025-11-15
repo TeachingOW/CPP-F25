496f3468eac52c14853e906732837aa7751b49dc
khalefa-ow <88914125+khalefa-ow@users.noreply.github.com>
2025-11-15 09:05:23 +0000

    unit test not yet working

--- code/unittest/_deps/cpputest-build/src/CppUTest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/CPP-F25/code/unittest/_deps/cpputest-src/src/CppUTest
-# Build directory: /workspaces/CPP-F25/code/unittest/_deps/cpputest-build/src/CppUTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- code/unittest/_deps/cpputest-build/src/CppUTestExt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/CPP-F25/code/unittest/_deps/cpputest-src/src/CppUTestExt
-# Build directory: /workspaces/CPP-F25/code/unittest/_deps/cpputest-build/src/CppUTestExt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
