bd4ae613b38fabfc80064a16b26aa7a6b8c0a045
oldwestbury <bestcodeguru@gmail.com>
2025-11-24 22:17:05 +0000

    arguments and bitmap

---
+++ code/bmp/progressbar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+int main(){
+std::cout << " 0";
+long long  max= 100000000;
+long long step=10000000;
+long l=0;
+for(long long i=0;i<max;i++){
+    
+    for(int j=0;j<10;j++){
+        l=l*l;
+    }
+    if (i%step==0){
+        std::cout << "\b\b";
+        std::cout << std::setprecision(2) << i*100/max ;
+
+    }
+}
+return l;
+
+}
